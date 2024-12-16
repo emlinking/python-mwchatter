af7ef0263dcd64b6a36a486decd3ee9bf0812eda
Eleanor Lin <elealin@curry.si.umich.edu>
2024-12-16 15:07:10 -0500

    move parse_talk_pages.sh, scrapeTalkPages.py

---
+++ parse_talk_pages.sh
@@ -0,0 +1,31 @@
+# parse_talk_pages.sh
+# Usage: parse_talk_pages.sh [zipped-filepath] [out-dir]
+
+ZIPFILE=$1
+
+UNZIPFILE="${ZIPFILE%.*}"
+
+OUTDIR=$2
+
+# unzip file
+if [ -f "$UNZIPFILE" ]; then
+    echo "The file '$UNZIPFILE' already exists. Skipping unzip."
+else
+    echo bzip2 -dk $ZIPFILE
+    bzip2 -dk $ZIPFILE
+fi
+
+# scrape file
+echo python ~/projects/code-switching/scrapeTalkPages.py $UNZIPFILE $OUTDIR
+python ~/projects/code-switching/scrapeTalkPages.py $UNZIPFILE $OUTDIR
+
+# Check if the scraping succeeded
+if [ $? -ne 0 ]; then
+    echo "Python script to scrape talk pages failed."
+    exit 1
+fi
+
+# delete unzipped file to save server space
+echo "Successfully completed scraping. Deleting '$UNZIPFILE' to save space."
+echo rm $UNZIPFILE
+rm $UNZIPFILE
---
+++ scrapeTalkPages.py
@@ -0,0 +1,62 @@
+# scrapeTalkPages.py
+# Eleanor Lin x ChatGPT
+# Usage: python scrapeTalkPages.py [dump-file] [out-dir]
+# out-dir should be a directory within which this script will make a subdirectory to store the results of scraping the XML from dump-file
+
+import json
+import mwxml
+import os
+import sys
+import tqdm
+import wikichatter as wc
+
+def main(fp, dir):
+    # read in dump file
+    dump = mwxml.Dump.from_file(open(fp))
+
+    # make subdirectory if it does not already exist
+    subdirectory = os.path.join(dir, fp.replace(".xml", ""))
+
+    if not os.path.isdir(subdirectory):
+        # Create the subdirectory if it does not exist
+        os.makedirs(subdirectory)
+
+        print(f"Subdirectory '{subdirectory}' created.")
+    else:
+        print(f"Subdirectory '{subdirectory}' already exists.")
+
+    # loop over pages in dump file
+    for page in tqdm.tqdm(dump):
+        
+        # grab only the Talk and User talk pages
+        if page.namespace in {1,3}:
+
+            # grab most recent revision
+            revision = None
+
+            for revision in page:
+                pass
+
+            # create filename that saves key info about this page and revision
+            save_path = "{}_{}_{}_{}_{}_{}_{}".format(revision.page.title,
+                                      revision.page.id,
+                                      revision.namespace,
+                                      revision.id, 
+                                      revision.timestamp, 
+                                      revision.user.text, 
+                                      revision.user.id).replace(":", "-")
+            
+            save_path = os.path.join(subdirectory, save_path)
+            
+            # parse the page
+            parse = wc.parse(revision.text)
+
+            # save the parse
+            # https://www.geeksforgeeks.org/how-to-convert-python-dictionary-to-json/#convert-dictionary-in-python-to-json-file-using-jsondump
+            with open(save_path, "w") as outfile:
+                json.dump(parse, outfile)
+
+if __name__=="__main__":
+    fp, dir = sys.argv[1], sys.argv[2]
+
+    main(fp, dir)
