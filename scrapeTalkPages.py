90d10074a2b970381e4da81cb3a7ef01f7a9ea8b
Eleanor Lin <emlinking@gmail.com>
2024-12-16 22:46:47 -0500

    add debug message

--- scrapeTalkPages.py
+++ scrapeTalkPages.py
@@ -41,6 +41,8 @@ def main(fp, dir):
     for page in tqdm.tqdm(dump, total=pages):
         
         # grab only the Talk and User talk pages
+        if page.id in processed:
+            print("skipped")
         if (page.namespace in {1,3}) and (page.id not in processed):
 
             # grab most recent revision
