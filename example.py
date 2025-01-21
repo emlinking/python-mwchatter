d98f44cba6e0ee65b4402d97ff16aeee04123d3b
Eleanor Lin <emlinking@gmail.com>
2025-01-21 12:45:43 -0500

    add back pretty printing

--- example.py
+++ example.py
@@ -10,10 +10,10 @@ for (name, directories, files) in os.walk(talk_samples_base):
 for f_path in talk_files:
     if f_path.endswith(".json"):
         continue
-    
+
     with open(f_path, "r") as f:
         text = f.read()
         parsed = wc.parse(text)
 
         with open(f_path.replace(".txt", ".json"), "w") as json_file:
-            json.dump(parsed, json_file)
+            json.dump(parsed, json_file, indent=4)
