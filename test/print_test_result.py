719c82fd29985d41affc00cd85eff735624df545
Eleanor Lin <emlinking@gmail.com>
2025-01-25 19:53:17 -0500

    add section-printing function

--- test/print_test_result.py
+++ test/print_test_result.py
@@ -2,14 +2,18 @@ import json
 import pprint
 import argparse
 
-def read_and_pretty_print_json(file_path):
+def read_and_pretty_print_json(file_path, head):
     with open(file_path, 'r') as file:
         data = json.load(file)
-        pprint.pprint(data)
+
+        for section in data["sections"]:
+            if section["heading"] == head:
+                pprint.pprint(section)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Read and pretty print a JSON file.')
     parser.add_argument('file_path', type=str, help='The path to the JSON file')
+    parser.add_argument('section', type=str, help="Name of section to display")
     args = parser.parse_args()
     
-    read_and_pretty_print_json(args.file_path)
+    read_and_pretty_print_json(args.file_path, args.section)
